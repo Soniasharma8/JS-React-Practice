68407d2f9a2b27a28dd3f16fdd8f2fc66c2b4a45
Sonia Sharma <Soniashar2021@gmail.com>
2025-08-09 21:33:44 +0530

    first commit

---
+++ Day-1/script.js
@@ -0,0 +1,9 @@
+// var fullName  = "Sonia";
+// let age = 23;
+// const country = "India";
+// console.log("My Name is "+ fullName + "," + " I am " + age + " years old," + " and I live in " + country)
+
+
+
+
+
