b181da02baf3e192dd1ad2014a89bf469242c23f
Sonia Sharma <Soniashar2021@gmail.com>
2025-08-10 19:21:38 +0530

    push

--- Day-1/script.js
+++ Day-1/script.js
@@ -4,6 +4,46 @@
 // console.log("My Name is "+ fullName + "," + " I am " + age + " years old," + " and I live in " + country)
 
 
+// let marks = 49;
+// if (marks >= 90) {
+//     console.log("Grade A");
+// }
+// else if(marks >= 70 && marks < 90) {
+//     console.log("Grade B");
+// }
+// else if (marks >= 50 && marks < 70) {
+//     console.log("Grade C");
+// }
+// else {
+//     console.log("Fail");
+// }
+
+// let colors = ["red", "green", "blue", "yellow"];
+// for (let i = 0; i < colors.length; i++) {   
+//     console.log(colors[i])
+// }
+
+// for(let color of colors) {
+//     console.log(color)
+// }
+
+// colors.forEach(function(color) {
+//     console.log(color)
+// })
+
+
+// function square(number) {
+//     console.log(`updated, ${number * number}`)
+// }
+// square(5);
+
+// function sum(number) {
+//     let sum = 0;
+//     for(let i = 0; i <= number; i++) {
+//         sum += i;
+//         }
+// }
+// sum(5);
 
 
 
